25d5a2eca598cbeae12324c7068938ed5ff2baf6
Tom White <tom@sixdozen.com>
2015-09-22 01:24:15 -0700

    cleanup of dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 from __future__ import division, print_function
 
 import logging
@@ -16,8 +16,6 @@ import ipdb
 import time
 import cPickle as pickle
 
-#import blocks.extras
-
 from argparse import ArgumentParser
 from theano import tensor
 
@@ -29,7 +27,7 @@ from blocks.algorithms import GradientDescent, CompositeRule, StepClipping, RMSP
 from blocks.bricks import Tanh, Identity
 from blocks.bricks.cost import BinaryCrossEntropy
 from blocks.bricks.recurrent import SimpleRecurrent, LSTM
-from blocks.initialization import Constant, IsotropicGaussian, Orthogonal 
+from blocks.initialization import Constant, IsotropicGaussian, Orthogonal
 from blocks.filter import VariableFilter
 from blocks.graph import ComputationGraph
 from blocks.roles import PARAMETER
@@ -37,7 +35,6 @@ from blocks.monitoring import aggregation
 from blocks.extensions import FinishAfter, Timing, Printing, ProgressBar
 from blocks.extensions.saveload import Checkpoint
 from blocks.extensions.monitoring import DataStreamMonitoring, TrainingDataMonitoring
-#from blocks.extras.extensions.plot import Plot
 from blocks.main_loop import MainLoop
 from blocks.model import Model
 
@@ -58,6 +55,7 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
     total_height = rows * height + (rows - 1)
     total_width  = cols * width + (cols - 1)
 
+    # logic for borders
     I = np.zeros((channels, total_height, total_width))
     two_rows_over = height * 2 + 2
     three_cols_over = width * 3 + 3
@@ -71,19 +69,22 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
     # I[0][:,:two_rows_height].fill(0.5)
     # I[0][:,-two_rows_height:].fill(0.5)
 
+    # training pairs at top
     if top_pairs is not None:
         for c in range(cols):
             for r in range(2):
                 offset_y, offset_x = r * height + r, c * width + c
                 I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = top_pairs[c][r]
 
+    # test pairs at bottom
     if bottom_pairs is not None:
         for c in range(cols):
             for i in range(2):
                 r = rows - i - 1
                 offset_y, offset_x = r * height + r, c * width + c
                 I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = bottom_pairs[c][i]
-    
+
+    # random samples in the center
     sample_rows = rows - 4
     sample_cols = cols - 6
     if samples is not None:
@@ -95,6 +96,7 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
                 offset_y, offset_x = cur_r * height + cur_r, cur_c * width + cur_c
                 I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = samprect[c][r]
 
+    # nearest neighbors on the left
     if left_samp is not None:
         for c in range(3):
             cur_c = c
@@ -103,6 +105,7 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
                 offset_y, offset_x = cur_r * height + cur_r, cur_c * width + cur_c
                 I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = left_samp[r][c]
 
+    # nearest neighbors on the right
     if right_samp is not None:
         for c in range(3):
             cur_c = cols - c - 1
@@ -111,12 +114,7 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
                 offset_y, offset_x = cur_r * height + cur_r, cur_c * width + cur_c
                 I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = right_samp[r][c]
 
-    # cur_c = cols-2
-    # for r in range(sample_rows):
-    #     cur_r = r + 2
-    #     offset_y, offset_x = cur_r * height + cur_r, cur_c * width + cur_c
-    #     I[0:channels, offset_y:(offset_y+height), offset_x:(offset_x+width)] = right_samp[r]
-
+    # convert everything to image
     if(channels == 1):
         out = I.reshape( (total_height, total_width) )
     else:
@@ -129,10 +127,11 @@ def render_grid(rows, cols, height, width, channels, top_pairs, bottom_pairs, sa
 
     return Image.fromarray(out)
 
+# build up a set of reconstructed pairs (training or test)
 def build_reconstruct_pairs(data_stream, num, model, channels, size):
     draw = model.get_top_bricks()[0]
     x = tensor.matrix('features')
-    reconstruct_function = theano.function([x], draw.reconstruct(x))    
+    reconstruct_function = theano.function([x], draw.reconstruct(x))
     iterator = data_stream.get_epoch_iterator(as_dict=True)
     pairs = []
     target_shape = (channels, size, size)
@@ -150,21 +149,12 @@ def build_reconstruct_pairs(data_stream, num, model, channels, size):
     for i in range(num):
         next_im = datastream_images[i].reshape(input_shape)
         recon_im, kterms = reconstruct_function(next_im)
-        # EXPERIMENT recon_im, kterms, z, u = reconstruct_function(next_im)
-        # print(kterms.shape, np.amin(kterms), np.amax(kterms))
-        # print("Z", z.shape, z[0], z[1], np.amin(z), np.amax(z))
-        # print("U", u.shape, u[0], u[1], np.amin(u), np.amax(u))
-        pairs.append([next_im.reshape(target_shape), 
+        pairs.append([next_im.reshape(target_shape),
                       recon_im.reshape(target_shape)])
 
-    # for i in range(num):
-    #     batch = next(iterator)
-    #     next_im = batch['features']
-    #     recon_im, kterms = reconstruct_function(next_im)
-    #     pairs.append([next_im.reshape(target_shape), 
-    #                   recon_im.reshape(target_shape)])
     return pairs
 
+# random samples
 def build_random_samples(model, num_rand, size, channels):
     # reset the random generator
     draw = model.get_top_bricks()[0]
@@ -188,9 +178,11 @@ def build_random_samples(model, num_rand, size, channels):
     target_shape = (niter, num_rand, channels, size, size)
     return samples.reshape(target_shape)
 
+# image_diff - used to caluclate nearest training neighbor
 def get_image_diff(im1, im2):
     return np.linalg.norm(im1 - im2)
 
+# find nearest 2 neighbors in data_stream for each target
 def gen_match_pairs(data_stream, size, channels, targets):
     target_shape = (channels, size, size)
     matches = []
@@ -224,12 +216,13 @@ def gen_match_pairs(data_stream, size, channels, targets):
 
     return matches
 
+# add reconstruction for nearest neighbor
 def attach_recon_to_neighbors(model, images_left, images_right, channels, size):
     logging.info("Compiling reconstruction function...")
     draw = model.get_top_bricks()[0]
     x = tensor.matrix('features')
     x_recons, kl_terms = draw.reconstruct(x)
-    reconstruct_function = theano.function([x], draw.reconstruct(x))    
+    reconstruct_function = theano.function([x], draw.reconstruct(x))
 
     input_shape = (1, channels * size * size)
     target_shape = (channels, size, size)
@@ -237,40 +230,42 @@ def attach_recon_to_neighbors(model, images_left, images_right, channels, size):
     for n in images_left:
         next_im = n[1].reshape(input_shape)
         recon_im, kterms = reconstruct_function(next_im)
-        n.append(recon_im.reshape(target_shape))        
+        n.append(recon_im.reshape(target_shape))
 
     for n in images_right:
         next_im = n[1].reshape(input_shape)
         recon_im, kterms = reconstruct_function(next_im)
-        n.append(recon_im.reshape(target_shape))        
+        n.append(recon_im.reshape(target_shape))
 
-
+# build all neighbors (left and right side with reconstructions)
 def build_neighbors(model, data_stream, size, channels, images_left, images_right):
     pairs_left = gen_match_pairs(data_stream, size, channels, images_left)
     pairs_right = gen_match_pairs(data_stream, size, channels, images_right)
     attach_recon_to_neighbors(model, pairs_left, pairs_right, channels, size)
     return [pairs_left, pairs_right]
 
+# generate entire dash and save
 def generate_dash(model, subdir, size, channels, lab, rows, cols, train_stream, test_stream):
     sample_rows = rows - 4
     sample_cols = cols - 6
     num_rand = sample_rows * sample_cols
 
-    logging.info("Generating random samples")    
+    logging.info("Generating random samples")
     samples = build_random_samples(model, num_rand, size, channels)
     left_samp = samples[-1][:sample_rows]
     right_samp = samples[-1][-sample_rows:]
     logging.info("Generating neighbors")
     left_pairs, right_pairs = build_neighbors(model, train_stream, size, channels, left_samp, right_samp)
-    logging.info("Generating pairs (training)")    
+    logging.info("Generating pairs (training)")
     train_pairs = build_reconstruct_pairs(train_stream, cols, model, channels, size)
-    logging.info("Generating pairs (test)")    
+    logging.info("Generating pairs (test)")
     test_pairs = build_reconstruct_pairs(test_stream, cols, model, channels, size)
     logging.info("Rendering grid")
     imgrid = render_grid(rows, cols, size, size, channels, train_pairs, test_pairs, samples, left_pairs, right_pairs, lab)
     imgrid.save("{0}/dash.png".format(subdir))
 
-def do_sample(subdir, args):
+# load model and run based on args
+def unpack_and_run(subdir, args):
     rows = args.rows
     cols = args.cols
     lab = args.lab
@@ -286,7 +281,7 @@ def do_sample(subdir, args):
         return
 
     dataset = args.dataset
-    logging.info("Loading dataset %s..." % dataset)    
+    logging.info("Loading dataset %s..." % dataset)
     image_size, channels, data_train, data_valid, data_test = datasets.get_data(dataset, args.channels, args.size)
     train_stream = Flatten(DataStream.default_stream(data_train, iteration_scheme=SequentialScheme(data_train.num_examples, 1)))
     test_stream  = Flatten(DataStream.default_stream(data_test,  iteration_scheme=SequentialScheme(data_test.num_examples, 1)))
@@ -295,6 +290,7 @@ def do_sample(subdir, args):
     generate_dash(model, subdir, size, channels, lab, rows, cols, train_stream, test_stream)
     # generate_samples(p, rows, cols, train_pairs, subdir, size, channels)
 
+# main - setup args, make output directory, and the run
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
@@ -319,4 +315,4 @@ if __name__ == "__main__":
     if not os.path.exists(subdir):
         os.makedirs(subdir)
 
-    do_sample(subdir, args)
+    unpack_and_run(subdir, args)
